734606cc54593a5f907af4d48fa2c18356e8f981
AllenCharles <cloudman@foxmail.com>
2020-06-01 18:43:11 +0800

    py

---
+++ leetcode/两数之和/answer.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) :
+        for i in nums:
+            for j in nums:
+                if i!=j:
+                    if i+j=target
+                    print(i,j)
+        
